407411391f9f24e8ded6fe1df935a302dee97602
Ahmad Siddique <ahmadsiddiquech@gmail.com>
2021-10-05 14:50:25 +0500

    deploy

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: "mbbxyimy_binary",
     password: "i*49ry1B*Mk]",
     database: "mbbxyimy_printingpress",
-    host: "162.241.244.49",
+    host: "162.241.244.49gir",
     port: 5432
 });
 
