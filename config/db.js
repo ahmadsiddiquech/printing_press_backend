965e1d5f9b47955ad6af2c57b3d94403f9569d95
Ahmad Siddique <ahmadsiddiquech@gmail.com>
2021-10-11 18:13:06 +0500

    heroku db

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     password: "2a7a4d981143b9859c9727c81434a9a3da6beb36a9bb21b86ce9640188a81c3b",
     database: "d6a53id3a5ajus",
     host: "ec2-54-173-31-84.compute-1.amazonaws.com",
-    port: "5432"
+    port: "5432",
 });
 
 module.exports = pool;
