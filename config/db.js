181728b33f80b0e710542949fcc3cb01080bfd48
Ahmad Siddique <ahmadsiddiquech@gmail.com>
2021-10-11 17:58:50 +0500

    heroku db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "mbbxyimy_binary",
-    password: "i*49ry1B*Mk]",
-    database: "mbbxyimy_printingpress",
-    host: "162.241.244.49",
+    user: "mzshaclvprkqtk",
+    password: "2a7a4d981143b9859c9727c81434a9a3da6beb36a9bb21b86ce9640188a81c3b",
+    database: "d6a53id3a5ajus",
+    host: "ec2-54-173-31-84.compute-1.amazonaws.com",
     port: 5432
 });
 
